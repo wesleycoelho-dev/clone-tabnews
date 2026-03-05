e078fd07cf0964771425d23aeed51a8186d348ae
wesleycoelho-dev <wesleycoelho.dev@gmail.com>
2026-03-05 00:11:33 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+function Home() {
+  return (
+    <div>
+      <h1>Deixando aqui uma musica bonitinha pra minha gatinha ₍^. .^₎Ⳋ!!</h1> 
+
+      <iframe
+        width="400"
+        height="225"
+        src="https://www.youtube.com/embed/N5TVWZfRfIs?start=154&end=210"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+
+    </div>
+  );
+}
+
+export default Home;
