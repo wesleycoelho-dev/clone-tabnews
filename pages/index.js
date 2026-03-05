6f9b0f01c57460c19390747193110731cf849b39
wesleycoelho-dev <wesleycoelho.dev@gmail.com>
2026-03-05 03:29:54 +0000

    alterando frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>Deixando aqui uma musica bonitinha pra minha gatinha ₍^. .^₎Ⳋ!!</h1> 
+      <h1>Deixando aqui uma musica bonitinha pra minha gatinha ₍^. .^₎Ⳋ!</h1> 
 
       <iframe
         width="400"
