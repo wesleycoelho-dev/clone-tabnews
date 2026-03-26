d933ef573d4bb457a8fe587751db02d9d3a0255d
Wesley <wesleycoelho.dev@gmail.com>
2026-03-26 21:50:50 +0000

    teste `teste`

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,17 @@
 function Home() {
   return (
     <div>
-      <h1>Deixando aqui uma musica bonitinha pra minha gatinha ₍^. .^₎Ⳋ!</h1> 
+      <h1>Deixando aqui uma musica bonitinha pra minha gatinha ₍^. .^₎!</h1>
 
       <iframe
         width="400"
         height="225"
         src="https://www.youtube.com/embed/N5TVWZfRfIs?start=154&end=210"
         title="YouTube video player"
-        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
       ></iframe>
-
     </div>
   );
 }
